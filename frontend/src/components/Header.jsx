744569064b66f428ffe163eec699e3b0688eb8c8
ceabruno <bruno.cea@inacapmail.cl>
2026-04-07 13:44:26 -0400

    cambie formato a webp para imagenes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 // 1. IMPORTAMOS EL LOGO
 // Fíjate cómo la ruta debe coincidir exactamente con el nombre de tu archivo
-import logoAncon from '../assets/Logo fondo chico.png'; 
+import logoAncon from '../assets/Logo fondo chico.webp'; 
 
 const UserIcon = () => (
   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
